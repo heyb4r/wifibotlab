bb572ef38887a477cb90e9b024c7966daf0caeea
Sight4 <sight4@gmail.com>
2019-04-28 14:47:01 +0200

    1st commit

---
+++ myrobot.h
@@ -0,0 +1,35 @@
+#ifndef MYTCPCLIENT_H
+#define MYTCPCLIENT_H
+
+#include <QObject>
+#include <QTcpSocket>
+#include <QAbstractSocket>
+#include <QDebug>
+#include <QTimer>
+#include <QMutex>
+
+class MyRobot : public QObject {
+    Q_OBJECT
+public:
+    explicit MyTcpClient(QObject *parent = 0);
+    void doConnect();
+    void disConnect();
+    QByteArray DataToSend;
+    QByteArray DataReceived;
+    QMutex Mutex;
+
+signals:
+    void updateUI(const QByteArray Data);
+public slots:
+    void connected();
+    void disconnected();
+    void bytesWritten(qint64 bytes);
+    void readyRead();
+    void MyTimerSlot();
+
+private:
+    QTcpSocket *socket;
+    QTimer *TimerEnvoi;
+};
+
+#endif // MYTCPCLIENT_H
